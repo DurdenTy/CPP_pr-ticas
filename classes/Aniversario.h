82c6de13e5618665d9d92de4eaab8a9e9b6b59f5
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-04-24 14:37:49 -0300

    Create Aniversario.h

---
+++ classes/Aniversario.h
@@ -0,0 +1,14 @@
+#ifndef ANIVERSARIO_H
+#define ANIVERSARIO_H
+
+
+class Aniversario
+{
+	public:
+		Aniversario(int d, int m, int a);
+		void mostrarData();
+	private:
+		int dia, mes, ano;
+};
+
+#endif
