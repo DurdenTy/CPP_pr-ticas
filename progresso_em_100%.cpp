6aae5f1107311fa1fafbea0ca2d53343d913d4b3
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-04-29 12:57:02 -0300

    Create progresso_em_100%.cpp
    Programa simples para criar uma barra de progresso simples

---
+++ progresso_em_100%.cpp
@@ -0,0 +1,17 @@
+int main(){
+	int contador2 = 0;
+	int contador = 0;
+	char x = -2;
+	for(int i = 1; i <= 50; i++){
+		system("cls");
+		contador -= 2;
+		contador2 += 2;
+		for(int u = 0; u < i; u++){
+			cout << x;
+		}
+		cout << " " << contador2 << "%" << endl;
+		
+	}
+	
+	return 0;
+}
